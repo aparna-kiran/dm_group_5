6a70d06abe032eaee2e63be348dc3cba744c7d43
Aparna Kiran A S <aparnakiran.as@gmail.com>
2024-03-15 23:18:41 +0000

    Renaming the datasets and adding the datasets into the SQL database

---
+++ R/load_data.R
@@ -0,0 +1,47 @@
+library(readr)
+library(RSQLite)
+
+data_files <- list.files("data_uploads/Dataset/")
+
+suffix <- "-Table 1"
+
+# Rename files
+for (file in data_files) {
+  # Create a new filename
+  new_filename <- paste0("data_uploads/Dataset/", gsub(suffix, "", file))
+  file <- paste0("data_uploads/Dataset/", file)
+  # Rename the file
+  file.rename(from = file, to = new_filename)
+}
+
+data_files <- list.files("data_uploads/Dataset/")
+
+db_connection <- RSQLite::dbConnect(RSQLite::SQLite(),"ecommerce.db")
+
+# To display the rows and columns of each dataset and 
+# To import each csv file into the database table
+for (file in data_files) {
+  this_filepath <- paste0("data_uploads/Dataset/", file)
+  this_file_contents <- readr::read_csv(this_filepath)
+  
+  number_of_rows <- nrow(this_file_contents)
+  number_of_columns <- ncol(this_file_contents)
+  
+  #To print the number of columns and rows of each dataset
+  print(paste0("The file: ",file,
+               " has: ",
+               format(number_of_rows,big.mark = ","),
+               " rows and ",
+               number_of_columns," columns"))
+  
+  table_name <- gsub(".csv","",file)
+  
+  #Writing the csv file contents to the database and
+  #creating the table with the table_name
+  RSQLite::dbWriteTable(db_connection,table_name,this_file_contents,overwrite=TRUE)
+  
+  #To list the database tables
+  RSQLite::dbListTables(db_connection)
+}
+
+RSQLite::dbDisconnect(db_connection)
