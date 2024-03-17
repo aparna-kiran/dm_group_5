5a43873345db5dfb6686be63ca8624f9e70a7d23
Xiaolu1011 <jane.zxl1011@gamil.com>
2024-03-17 17:44:37 +0000

    changes

--- R/data_validation.R
+++ R/data_validation.R
@@ -1,15 +1,47 @@
 library(readr)
 
 # Get a list of file names in the directory
-dataset <- list.files("project/data_uploads/Dataset/")
-dataset
+data_files <- list.files("data_uploads/")
+data_files
 
-#the tables by removing the suffix "-Table 1.csv"
+# To import each csv file into the database table
+for (file in data_files) {
+  this_filepath <- paste0("data_uploads/", file)
+  this_file_contents <- readr::read_csv(this_filepath)
+  
+  number_of_rows <- nrow(this_file_contents)
+  number_of_columns <- ncol(this_file_contents)
+  
+  #To print the number of columns and rows of each dataset
+  print(paste0("The file: ",file,
+               " has: ",
+               format(number_of_rows,big.mark = ","),
+               " rows and ",
+               number_of_columns," columns"))
+  
+  table_name <- gsub(".csv","",file)
+}
+
+# Check if the first column of each file is a primary
+for (variable in data_files) {
+  this_filepath <- paste0("data_uploads/", variable)
+  this_file_contents <- readr::read_csv(this_filepath)
+  number_of_rows <- nrow(this_file_contents)
+  
+  print(paste0("Checking for: ", variable))
+  
+  print(paste0(" is ", nrow(unique(this_file_contents[, 1]))==number_of_rows))
+}
+
+#simplify the tables by removing the suffix "-Table 1.csv"
 suffix <- "-Table 1.csv"
-dataset <- gsub("-Table 1.csv","",datasets)
-dataset
+data_files <- gsub("-Table 1.csv","", data_files)
+data_files
 
 #Check duplicates (its not that necessary)
+
+#Check email format
+
 
 #import to database
 
