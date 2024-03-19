af38dc319ff9e0be5da57266cc2d15e77ab08edd
elifcaysar <elif.caysar@warwick.ac.uk>
2024-03-19 14:46:35 +0000

    Commit pull for the first time

--- R/load_data.R
+++ R/load_data.R
@@ -38,7 +38,6 @@ for (file in data_files) {
   
   #Writing the csv file contents to the database and
   #creating the table with the table_name
-  RSQLite::dbWriteTable(db_connection,table_name,this_file_contents,overwrite=TRUE)
   
   #To list the database tables
   RSQLite::dbListTables(db_connection)
